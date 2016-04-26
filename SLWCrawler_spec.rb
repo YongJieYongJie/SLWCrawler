fb36164c09f1298da9f97c9e3efb8d61e3a6a2a7
YongJieYongJie <KhooYongJie@gmx.com>
2016-04-27 02:13:19 +0800

    Adds test for scraping of judgments from page source

--- SLWCrawler_spec.rb
+++ SLWCrawler_spec.rb
@@ -12,10 +12,38 @@ describe SLWCrawler do
 
   it 'scrapes website into array of judgments' do
     VCR.use_cassette('fetch_website') do
-      response = SLWCrawler.fetch_website
-      judgments = SLWCrawler.scrape_into_judgments(response.body)
-      puts judgments.inspect
-      expect(judgments).not_to be_nil
+      website_source = %(
+<ul id="judgments-list">
+  <li>
+    <span id="catchwords">
+      Insolvency law — Bankruptcy
+    </span>
+    <br>
+    <a href="http://www.singaporelawwatch.sg/slw/index.php/component/cck/?task=download&amp;file=attached_document&amp;id=80783&amp;utm_source=web%20subscription&amp;utm_medium=web&amp;src=judgments">
+      Lee Han Min Garry v Piong Michelle Lucia
+    </a>
+    <br>
+    <span id="link-pdf">
+      [2016] SGHC 79 |   Decision Date: 21 Apr 2016
+    </span>
+  </li>
+  <li>
+    <span id="catchwords">
+      Arbitration — Agreement, Arbitration — Arbitrability and public policy, Arbitration — Stay of court proceedings
+    </span>
+    <br>
+    <a href="http://www.singaporelawwatch.sg/slw/index.php/component/cck/?task=download&amp;file=attached_document&amp;id=80782&amp;utm_source=web%20subscription&amp;utm_medium=web&amp;src=judgments">
+      Maniach Pte Ltd v L Capital Jones Ltd and another
+    </a>
+    <br>
+    <span id="link-pdf">
+      [2016] SGHC 65 |   Decision Date: 26 Apr 2016
+    </span>
+  </li>
+</ul>
+)
+      judgments = SLWCrawler.scrape_into_judgments(website_source)
+      expect(judgments.count).to eq(2)
     end
   end
 
