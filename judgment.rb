a9aec4a0e0e409d955458738c9fe718c5330ac9c
YongJieYongJie <KhooYongJie@gmx.com>
2016-04-27 02:10:29 +0800

    Adds name shortening regexp

--- judgment.rb
+++ judgment.rb
@@ -1,11 +1,16 @@
-class Judgment < Struct.new(:case_name, :neutral_citation, :decision_date, :catchwords, :url)
-  attr_accessor :case_name, :neutral_citation, :decision_date, :catchwords, :url
-
+Judgment = Struct.new(:case_name, :neutral_citation, :decision_date, :catchwords, :url) do
   def initialize(args)
-    super(*args.values_at(:case_name, :neutral_citation, :decision_date, :catchwords, :url))
+	super(*args.values_at(:case_name, :neutral_citation, :decision_date, :catchwords, :url))
   end
 
   def generate_filename
-    "#{@case_name.gsub(/[\\\/:\*\?"<>|]/, '_')}, #{@neutral_citation}.pdf"
+	# replaces illegal characters \/:*?"<> with underscore
+	sanitized_case_name = case_name.to_s.gsub(/[\\\/:\*\?"<>|]/, '_') 
+	condensed_case_name = sanitized_case_name.gsub(/( and another( suit| matter| appeal)?| and others)/i, '')
+    condensed_case_name.gsub!(/ private limited| pte limited| pte ltd| pvt ltd| limited| ltd/i, '')
+    condensed_case_name.gsub!(/public prosecutor/i, 'PP')
+    condensed_case_name.gsub!(/ \([^\(]*?(liquidation|executrix|formerly|trading)[^\)]*?\)/i, '')
+    condensed_case_name.gsub!(/\s+/, ' ')
+	"#{condensed_case_name}, #{neutral_citation}.pdf"
   end
 end
