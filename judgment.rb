92695a1fd6f74b776e09da04d445181cbb10a04f
YongJieYongJie <KhooYongJie@gmx.com>
2016-04-30 20:30:12 +0800

    Removes filename sanitization from judgment.rb

--- judgment.rb
+++ judgment.rb
@@ -3,10 +3,8 @@ Judgment = Struct.new(:case_name, :neutral_citation, :decision_date, :catchwords
 	super(*args.values_at(:case_name, :neutral_citation, :decision_date, :catchwords, :url))
   end
 
-  def generate_filename
-	# replaces illegal characters \/:*?"<> with underscore
-	sanitized_case_name = case_name.to_s.gsub(/[\\\/:\*\?"<>|]/, '_') 
-	condensed_case_name = sanitized_case_name.gsub(/( and another( suit| matter| appeal)?| and others)/i, '')
+  def get_condensed_case_name
+	condensed_case_name = case_name.gsub(/( (and|&) (an)?other( suit| matter| appeal)?s?| and others| (and|&) [0-9] ors)/i, '')
     condensed_case_name.gsub!(/ private limited| pte limited| pte ltd| pvt ltd| limited| ltd/i, '')
     condensed_case_name.gsub!(/public prosecutor/i, 'PP')
     condensed_case_name.gsub!(/ \([^\(]*?(liquidation|executrix|formerly|trading)[^\)]*?\)/i, '')
