b1227b8bc96b43635776d8a91a94b8d1c597d27d
YongJieYongJie <KhooYongJie@gmx.com>
2016-05-01 17:47:32 +0800

    Changes tabs to spaces

--- judgment.rb
+++ judgment.rb
@@ -1,14 +1,14 @@
 Judgment = Struct.new(:case_name, :neutral_citation, :decision_date, :catchwords, :url) do
   def initialize(args)
-	super(*args.values_at(:case_name, :neutral_citation, :decision_date, :catchwords, :url))
+    super(*args.values_at(:case_name, :neutral_citation, :decision_date, :catchwords, :url))
   end
 
   def get_condensed_case_name
-	condensed_case_name = case_name.gsub(/( (and|&) (an)?other( suit| matter| appeal)?s?| and others| (and|&) [0-9] ors)/i, '')
+    condensed_case_name = case_name.gsub(/( (and|&) (an)?other( suit| matter| appeal)?s?| and others| (and|&) [0-9] ors)/i, '')
     condensed_case_name.gsub!(/ private limited| pte limited| pte ltd| pvt ltd| limited| ltd/i, '')
     condensed_case_name.gsub!(/public prosecutor/i, 'PP')
     condensed_case_name.gsub!(/ \([^\(]*?(liquidation|executrix|formerly|trading)[^\)]*?\)/i, '')
     condensed_case_name.gsub!(/\s+/, ' ')
-	condensed_case_name
+    condensed_case_name
   end
 end
